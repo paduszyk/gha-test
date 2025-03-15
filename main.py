f734af7cfe9404aedd302f4c798f058202483f1a
Kamil Paduszyński <92403542+paduszyk@users.noreply.github.com>
2025-03-15 21:30:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 def main():
     data = os.environ["DATA"]
 
-    data = dict((item.split("=") for item in filter(None, data.split("\n"))))
+    data = dict((map(str.strip, item.split("=")) for item in filter(None, data.split("\n"))))
 
     print(data)
 
